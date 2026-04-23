f1e55d76eeba1d6474d730fea2afa6bf10d6495e
robohie <rogerbosolinndo34@gmail.com>
2026-04-23 18:35:24 +0100

    modifications supplémentaires

--- collecte_donnees_audio.py
+++ collecte_donnees_audio.py
@@ -1,38 +1,50 @@
+# Importation des modules :
+# 1. sounddevice pour l'enregistrement des sons issus du microphone de l'ordinateur
+# 2. scipy.io.wavfile pour la lecture et l'écriture des fichiers enregistrement au format wav
+# 3. numpy pour le calcul matriciel
+# 4. os pour la gestion du système des fichiers
+# 5. time pour la gestion du temps
+
 import sounddevice as sd
 import scipy.io.wavfile as wav
 import numpy as np
 import os, time
 
-FREQUENCE_ECHANTILLON = 16000
-DUREE = 1 # Chaque enregistrement dure 1 seconde
-NOMBRE_CLIPS = 400
-NOM_CLASSE = "bruit"
-DOSSIER = f"sons/{NOM_CLASSE}"
+FREQUENCE_ECHANTILLON = 16000    # Le signal sonore est échantillonné à 16 kHz
+DUREE = 1                        # Chaque enregistrement dure 1 seconde, utile pour la conversion Mel
+NOMBRE_CLIPS = 400               # Le nombre d'individus par échantillon ou classe
+NOM_CLASSE = "bruit"             # La classe ou l'étiquette concernée
+DOSSIER = f"sons/{NOM_CLASSE}"   # Le sous dossier du dossier "sons" contenant les enregistrements
 
 os.makedirs(DOSSIER, exist_ok=True)
 print(f"Enregistrement de {NOMBRE_CLIPS} clips pour la classe '{NOM_CLASSE}'")
-print(f"format : {FREQUENCE_ECHANTILLON} Hz, 16 bits, mono, {DUREE}s")
-print("Prononcez le mot (appuyez sur ENTREE)")
+print(f"({FREQUENCE_ECHANTILLON} Hz, 16 bits, mono, {DUREE}s)")
+print('______________________________________________________________________\n')
+
 time.sleep(1)
 
-for i in range(NOMBRE_CLIPS):
-    input(f"[{i + 1:03d}/{NOMBRE_CLIPS}] Appuyez sur ENTREE puis prononcez '{NOM_CLASSE}'...")
-    audio = sd.rec(int(DUREE * FREQUENCE_ECHANTILLON),
-                   samplerate=FREQUENCE_ECHANTILLON,
-                   channels=1,
-                   dtype='int16')
+i = 0
+while i < NOMBRE_CLIPS:
+    if NOM_CLASSE.lower() != "bruit":
+        input(f"[{i + 1:03d}/{NOMBRE_CLIPS}] Appuyez sur ENTREE puis prononcez '{NOM_CLASSE}'...")
+    enregistrement = sd.rec(int(DUREE * FREQUENCE_ECHANTILLON),
+                            samplerate=FREQUENCE_ECHANTILLON,
+                            channels=1,
+                            dtype='int16')
     sd.wait()
 
     # Vérification du volume
-    volume = np.abs(audio).mean()
+    volume = np.abs(enregistrement).mean() # la moyenne des valeurs absolues des intensités
     if volume < 50:
         print(" [ATTENTION] Volume très faible. Recommencez ce clip!")
         i-=1
         continue
 
-    fichier = os.path.join(DOSSIER, f"{NOM_CLASSE}_{i:03d}.wav")
-    wav.write(fichier, FREQUENCE_ECHANTILLON, audio)
-    print(f"Sauvegardé : {fichier} (volume moyen: {volume:.0f})")
+    fichier = os.path.join(DOSSIER, f"{NOM_CLASSE}_{i+1:03d}.wav")
+    wav.write(fichier, FREQUENCE_ECHANTILLON, enregistrement)
+    print(f"{fichier} sauvegardé (volume moyen: {volume:.0f})")
+
+    i+=1
     time.sleep(0.3)
 
-print(f"\nTerminé! {NOMBRE_CLIPS} clips sauvegardés dans {DOSSIER}")
+print(f"\nEnregistrement terminé! {NOMBRE_CLIPS} clips sauvegardés dans {DOSSIER}")
--- train_model.py
+++ train_model.py
@@ -1,118 +1,48 @@
-import os, librosa
-import numpy as np
+# Importation des modules :
+# 1. os : gestion du système de fichiers, communication avec le S.E
+# 2. librosa : traitement des signaux musicaux ou de l'audio
+# 3. tensorflow : conception du modèle deep-learning par des réseaux des neurones
+# 4. scikit-learn(sklearn) : data analysis
+
 import tensorflow as tf
-from tensorflow.keras import layers, models
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+import os
 
-SR = 16000
-N_MFCC=13
-MAX_FRAMES = 100
-DONNEES_CHEMIN = "sons"
-BATCH_SIZE = 32
-EPOCHS = 30
 
-# 1. Charger tous les fichiers et leurs labels
-def extract_mfcc(chemin):
-    y, sr = librosa.load(chemin, sr=SR, mono=True)
-    if len(y) > SR:
-        y = y[:SR]
-    else:
-        y = np.pad(y, (0, SR - len(y)))
+def audio_vers_mfcc(chemin_audio):
+    """Cette fonction convertie les fichiers audio wav
+    en spectrogramme de Mel afin d'utiliser le réseau
+    des neurones convolutifs
+    """
+    # 1.CHARGEMENT DU FICHIER AUDIO
+    audio_binary = tf.io.read_file(chemin_audio)
+        # conversion des bytes vers un tableau de flottants
+    audio, _ = tf.audio.decode_wav(audio_binary,
+                                   desired_channels=1,
+                                   desired_samples=16000)
+    audio = tf.squeeze(audio, axis=-1) # supprime la dimension "canaux"([16000,1]->[16000])
 
-    mfcc = librosa.feature.mfcc(y=y, sr=SR, n_mfcc=N_MFCC, n_fft=512, hop_length=160)
-    mfcc = mfcc.T
+    # 2.CALCUL DE LA TRANSFORMEE DE FOURIER A COURT TERME (STFT)
+    stft = tf.signal.stft(
+        audio,
+        frame_length=640, # longueur de chaque fenêtre d'analyse
+        frame_step=320, # décalage entre fenêtres
+        fft_unique_bins=257 # nombre des fréquences analysées 257 = frame_length/2 + 1
+    )
 
-    if mfcc.shape[0] < MAX_FRAMES:
-        pad = np.zeros((MAX_FRAMES - mfcc.shape[0], N_MFCC))
-        mfcc = np.vstack([mfcc, pad])
-    else:
-        mfcc = mfcc[:MAX_FRAMES]
-    mfcc = (mfcc - mfcc.mean()) / (mfcc.std() + 1e-8)
-    return mfcc[..., np.newaxis].astype(np.float32)
+    # 3.CALCUL DU SPECTROGRAMME
+    spectrogramme = tf.abs(stft)
+    spectrogramme = tf.math.pow(spectrogramme, 0.5)
 
-x = []
-y = []
-print("Chargement des données...")
+    # 4. APPLICATION DES FILTRES MEL
+    num_mel_bins = 40 # nbre de bandes de fréquences Mel
+    linear_to_mel = tf.signal.linear_to_mel_weight_matrix(
+        num_mel_bins=num_mel_bins,
+        num_spectrogram_bins=257,
+        sample_rate=16000,
+        lower_edge_hertz=20.0, # fréquence minimale
+        upper_edge_hertz=4000 # fréquence maximale
+    )
+    mel_spectrogramme = tf.matmul(spectrogramme, linear_to_mel)
+    log_mel = tf.math.log(mel_spectrogramme + 1e-6)
 
-for mot in os.listdir(DONNEES_CHEMIN):
-    dossier = os.path.join(DONNEES_CHEMIN, mot)
-    if not os.path.isdir(dossier):
-        continue
-    for fichier in os.listdir(dossier):
-        if fichier.endswith(".wav"):
-            chemin_fichier = os.path.join(dossier, fichier)
-            try:
-                features = extract_mfcc(chemin_fichier)
-                x.append(features)
-                y.append(mot)
-            except Exception as e:
-                print(f"Erreur sur {chemin_fichier} : {e}")
-
-x, y = np.array(x), np.array(y)
-print(f"Données chargées : {x.shape} exemples")
-
-# 2. Encoder les labels
-le = LabelEncoder()
-y_int = le.fit_transform(y)
-y_cat = tf.keras.utils.to_categorical(y_int, num_classes=len(le.classes_))
-print("Classes : ", le.classes_)
-
-# 3. Split train / validation
-x_train, x_val, y_train, y_val = train_test_split(x, y_cat, test_size=0.2, random_state=42, stratify=y_int)
-
-# 4. Construction du modèle
-model = models.Sequential([
-        # Couche d'entrée
-        layers.Input(shape=(MAX_FRAMES, N_MFCC, 1)),
-
-        # Première convolution : detecte les motifs simples
-        layers.Conv2D(8, (5, 3), activation="relu", padding="same"),
-        layers.MaxPooling2D((2, 2)),
-
-        # Deuxième convolution
-        layers.Conv2D(16, (3, 3), activation="relu", padding="same"),
-        layers.MaxPooling2D((2, 2)),
-
-        # Transition vers les couches denses
-        layers.Flatten(),
-
-        layers.Dropout(0.25),
-
-        # Couche dense cachée
-        layers.Dense(64, activation="relu"),
-        # Couche de sortie
-        layers.Dense(len(le.classes_), activation="softmax")
-    ])
-model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-model.summary()
-
-# 5. Callbacks
-callbacks = [
-    tf.keras.callbacks.EarlyStopping(monitor="val_accuracy", patience=8, restore_best_weights=True),
-    tf.keras.callbacks.ModelCheckpoint('best_model.h5', monitor='val_accuracy', save_best_only=True),
-    tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=4, min_lr=1e-6)
-]
-
-# 6. Entraînement
-history = model.fit(
-    x_train, y_train,
-    validation_data=(x_val, y_val),
-    epochs=EPOCHS,
-    batch_size=BATCH_SIZE,
-    callbacks=callbacks,
-    verbose=1
-)
-
-# 7. Evaluation et sauvegarde
-val_loss, val_acc = model.evaluate(x_val, y_val, verbose=0)
-print(f"\nPrécision validation : {val_acc*100:.2f}%")
-
-# sauvegarde du modèle final
-model.save("modèle_vocal_final.h5")
-
-# sauvegarde des classes pour arduino
-with open("labels.txt", "w") as f:
-    for label in le.classes_:
-        f.write(label + "\n")
-print("Labels sauvegardés dans labels.txt (à copier dans le code Arduino)")
+    return log_mel # forme : (49, 40)
